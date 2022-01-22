edf0a76734c98173728e6372c0e2016a707d24b4
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-01-22 14:12:07 -0300

    Estilos Globais do App

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
-import styles from '../styles/home.module.scss'
+
+import Head from 'next/head'
 
 export default function Home() {
   return (
-   <h1 className={styles.title}>Hello World, Welcome to Publi.com</h1>
+    <>
+    <Head>
+       <title>Início | publi.com</title>
+    </Head>
+   <h1>
+     Hello World, Welcome to Publi.com
+   </h1>
+    </>
   )
 }
