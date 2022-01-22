103c0f46be392fb6fa5d1bab7aa4a82bc863e233
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-01-22 13:32:43 -0300

    Configuração _Document e Fontes Externas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-   <h1>Hello World, Welcome to Publi.com</h1>
+   <h1 className={styles.title}>Hello World, Welcome to Publi.com</h1>
   )
 }
