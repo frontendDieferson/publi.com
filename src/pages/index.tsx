0c671ce1c2433b0cc802ea01d707bdfdcd36fbec
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-01-22 18:11:02 -0300

    Consumindo API Stripe na Aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { stripe } from '../components/services/stripe'
+import { SubscribeButton } from '../components/SubscribeButton'
+
 
 import styles from './home.module.scss'
 
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
 
-export default function Home() {
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
     <Head>
@@ -17,12 +27,35 @@ export default function Home() {
         <span> Mundo</span> em 2022</h1>
         <p>
         Tenha acesso a todas as publicações <br />
-        <span>por R$9,90/mês</span>
+        <span>por {product.amount} /mês</span>
         </p>
+        <SubscribeButton priceId={product.priceId} />
       </section>
 
       <img src='/images/avatar.svg' alt='News World' />
+     
     </main>
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KKrUeH5XimrPhN5ehYsePB7', {
+   
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
