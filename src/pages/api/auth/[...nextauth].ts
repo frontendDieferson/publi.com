7339b87021f0dadb3e1488e16c2e02eb60b56d31
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-01-24 19:38:12 -0300

    Feature: Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -22,11 +22,27 @@ export default NextAuth({
        
        try {
         await fauna.query(
-            q.Create(
-                q.Collection('users'),
+           q.If(
+               q.Not(
+                   q.Exists(
+                       q.Match(
+                           q.Index('user_by_email'),
+                           q.Casefold(user.email)
+                       )
+                   )
+               ),
+               q.Create(
+                q.Collection('users'), 
                 { data: { email } }
+            ),
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                )
             )
         )
+     )
     
         return true
     } catch {
