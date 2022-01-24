41cace2062d88cf958092948607b8a750702baa7
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-01-24 19:21:48 -0300

    features: FaunaDB add

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,37 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-          params: {
-             prompt: 'consent',
-             access_type: 'offline',
-             response_type: 'code'
-          }
-      }
+      
+      
     }),
     
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+       const { email } = user
+       
+       try {
+        await fauna.query(
+            q.Create(
+                q.Collection('users'),
+                { data: { email } }
+            )
+        )
+    
+        return true
+    } catch {
+        return false
+     }
+    },
+}
 })
