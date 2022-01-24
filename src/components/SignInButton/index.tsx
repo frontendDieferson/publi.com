cd29b373bc14f7a8f7d8d42d3d1d7fecaa50e2dc
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-01-24 17:25:03 -0300

    features: NextAuth.js / Google

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,28 @@
 import { FaGoogle } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
         >
          <FaGoogle color='#04d361' />
-            Dieferson Soares
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : (
         <button 
         type="button"
         className={styles.signInButton}
+        onClick={() => signIn('google')}
         >
          <FaGoogle color='#eba417' />
           Entrar com Google
