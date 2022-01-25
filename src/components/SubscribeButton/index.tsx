0460754e52b45a720b8778208b8cb5b3221c2785
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-01-25 12:33:59 -0300

    Feature: sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,29 @@
+import { signIn, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
+
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton({ priceId } : SubscribeButtonProps) {
+    const { data: session } = useSession()
+
+    function handlesubscribe() {
+        if (!session) {
+            signIn('google')
+            return;
+        }
+
+        
+
+    }
+
     return (
         <button
          type="button"
          className={styles.subscribebutton}
+         onClick={handlesubscribe}
         >
          Inscreva-se agora
         </button>
